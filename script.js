51a403c5249b207a3c298fb5f82672f37b85aeb3
Iffat Hossain <iffathossain@Iffats-MacBook-Air.local>
2022-03-10 11:37:38 -0500

    created movie banners div and assigned click to banneer as buttons

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
  
+// iterate through the array creating divs with loop
+// use loop to create poster and information
+let bannerContainer = document.querySelector("#banner-container")
+
 fetch('https://ghibliapi.herokuapp.com/films')
-    .then(response=> response.json())
-    .then(data => console.log(data))
-   
-    
+  .then(response => response.json())
+  .then(data => {
+    renderResults(data)
+  })
     
 
-   
+function renderResults(movieData) {
+
+  movieData.forEach((movie) => {
+      const cardInfo = `
+        <div class="movieBanner">
+        <button class="movieButton">
+          <img src="${movie.image}"/>
+          </button>
+        </div>
+      `
+      bannerContainer.insertAdjacentHTML('beforeend', cardInfo)
+  })
+
+  let movieButtons = document.querySelectorAll(".movieButton")
+
+  movieButtons.forEach((movieButton) => {
+    movieButton.addEventListener('click', () => {
+      alert("clicked the poster")
+    })
+  })
+}
+  
